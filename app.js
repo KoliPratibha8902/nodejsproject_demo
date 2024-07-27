edd7d2348cfe50bfb1e83643f9d227e780f28da2
kolipratibha <kolipratibha2002@gmail.com>
2024-07-27 15:24:24 +0530

    Updated MongoDB connection to use Atlas and environment variables

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/registrationDB')
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.log('Failed to connect to MongoDB', err));
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch(err => console.log('Failed to connect to MongoDB Atlas', err));
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -40,7 +43,7 @@ app.listen(PORT, () => {
 });
 
 
-
+// require('dotenv').config();
 // const express = require('express');
 // const mongoose = require('mongoose');
 // const bodyParser = require('body-parser');
@@ -48,10 +51,8 @@ app.listen(PORT, () => {
 // const app = express();
 
 // // MongoDB connection
-// mongoose.connect('mongodb://localhost:27017/registrationDB', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// }).then(() => console.log('Connected to MongoDB'))
+// mongoose.connect('mongodb://localhost:27017/registrationDB')
+//   .then(() => console.log('Connected to MongoDB'))
 //   .catch(err => console.log('Failed to connect to MongoDB', err));
 
 // // Middleware
@@ -83,3 +84,48 @@ app.listen(PORT, () => {
 // app.listen(PORT, () => {
 //   console.log(`Server is running on port ${PORT}`);
 // });
+
+
+
+// // const express = require('express');
+// // const mongoose = require('mongoose');
+// // const bodyParser = require('body-parser');
+
+// // const app = express();
+
+// // // MongoDB connection
+// // mongoose.connect('mongodb://localhost:27017/registrationDB', {
+// //   useNewUrlParser: true,
+// //   useUnifiedTopology: true,
+// // }).then(() => console.log('Connected to MongoDB'))
+// //   .catch(err => console.log('Failed to connect to MongoDB', err));
+
+// // // Middleware
+// // app.use(bodyParser.urlencoded({ extended: true }));
+// // app.set('view engine', 'ejs');
+// // app.use(express.static('public'));
+
+// // // User model
+// // const User = require('./models/User');
+
+// // // Routes
+// // app.get('/', (req, res) => {
+// //   res.render('index');
+// // });
+
+// // app.post('/register', async (req, res) => {
+// //   const { username, password } = req.body;
+// //   const newUser = new User({ username, password });
+  
+// //   try {
+// //     await newUser.save();
+// //     res.send('Registration successful');
+// //   } catch (err) {
+// //     res.send('Error registering user');
+// //   }
+// // });
+
+// // const PORT = process.env.PORT || 3000;
+// // app.listen(PORT, () => {
+// //   console.log(`Server is running on port ${PORT}`);
+// // });
