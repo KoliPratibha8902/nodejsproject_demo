6a7b657f972a56cb0eb837f725d30df25f0ade68
KoliPratibha8902 <atrcpt2pk@email.com>
2024-07-27 11:51:53 +0530

    Initial commit of registration project

---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/registrationDB')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log('Failed to connect to MongoDB', err));
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// User model
+const User = require('./models/User');
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const newUser = new User({ username, password });
+  
+  try {
+    await newUser.save();
+    res.send('Registration successful');
+  } catch (err) {
+    res.send('Error registering user');
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+
+// const app = express();
+
+// // MongoDB connection
+// mongoose.connect('mongodb://localhost:27017/registrationDB', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }).then(() => console.log('Connected to MongoDB'))
+//   .catch(err => console.log('Failed to connect to MongoDB', err));
+
+// // Middleware
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.set('view engine', 'ejs');
+// app.use(express.static('public'));
+
+// // User model
+// const User = require('./models/User');
+
+// // Routes
+// app.get('/', (req, res) => {
+//   res.render('index');
+// });
+
+// app.post('/register', async (req, res) => {
+//   const { username, password } = req.body;
+//   const newUser = new User({ username, password });
+  
+//   try {
+//     await newUser.save();
+//     res.send('Registration successful');
+//   } catch (err) {
+//     res.send('Error registering user');
+//   }
+// });
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
